2ba59404e23f780a3059946d9a2b6ca8d85c1c9e
Naoko Shibasaki <naoko.shibasaki@smarthr.co.jp>
2022-07-15 18:31:58 +0900

    字は添えるだけ（添字）

---
+++ 03/03_02/hash2.rb
@@ -0,0 +1,5 @@
+woman = { :height => 160, :weight => 50 }
+
+p woman
+p woman[:height]
+p woman[:weight]
---
+++ 03/03_02/hash3.rb
@@ -0,0 +1,5 @@
+child = { height: 110, weight: 20 }
+
+p child
+p child[:height]
+p child[:weight]
